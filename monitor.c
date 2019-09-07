fb2dbe5e93d68d0c9a5d0bac9d8553118c2f9f14
WrightRocket <wright.keith@gmail.com>
2019-09-07 11:28:30 -0700

    Added time_str to output

--- monitor.c
+++ monitor.c
@@ -36,35 +36,34 @@
 #define ERROR_PRESSURE_FILE_GONE    5
 #define ERROR_PRESSURE_EVENT_UNK    6   
 
-char    content_str[SZ_CONTENT];
-char    time_str[SZ_TIME];
-struct  pollfd fds[SZ_IDX];
-char    *pressure_file[SZ_IDX];
-int     trigger_threshold_ms[SZ_IDX];
-int     tracking_window_ms[SZ_IDX];
+char content_str[SZ_CONTENT];
+char time_str[SZ_TIME];
+struct pollfd fds[SZ_IDX];
+char *pressure_file[SZ_IDX];
+int trigger_threshold_ms[SZ_IDX];
+int tracking_window_ms[SZ_IDX];
 
 void set_time_str() {
     time_t now;
     struct tm* tm_info;
 
-    time(&now); 
+    time(&now);
     tm_info = localtime(&now);
     strftime(time_str, SZ_TIME, "%Y-%m-%d %H:%M:%S ", tm_info);
 }
 
 void set_content_str(int psi_idx) {
-    int bytes_read;
     int fd;
 
     memset(&(content_str[0]), 0, SZ_CONTENT);
-    fd = open (pressure_file[psi_idx], O_NONBLOCK | O_RDONLY );
-    bytes_read = read(fd, content_str, SZ_CONTENT);
+    fd = open(pressure_file[psi_idx], O_NONBLOCK | O_RDONLY);
+    read(fd, content_str, SZ_CONTENT);
     close(fd);
 }
 
 void poll_pressure_events() {
     char distress_event[SZ_EVENT];
-    
+
     for (int i = 0; i < SZ_IDX; i++) {
 
         memset(&(distress_event[0]), 0, SZ_EVENT);
@@ -74,12 +73,15 @@ void poll_pressure_events() {
             exit(ERROR_PRESSURE_OPEN);
         }
         fds[i].events = POLLPRI;
-        snprintf(distress_event, SZ_EVENT, "some %d %d", trigger_threshold_ms[i] * MS_TO_MU, tracking_window_ms[i] * MS_TO_MU);
+        snprintf(distress_event, SZ_EVENT, "some %d %d",
+                trigger_threshold_ms[i] * MS_TO_MU,
+                tracking_window_ms[i] * MS_TO_MU);
         printf("\n%s distress_event:\n%s\n", pressure_file[i], distress_event);
         set_content_str(i);
         printf("%s content:\n%s\n", pressure_file[i], content_str);
         if (write(fds[i].fd, distress_event, strlen(distress_event) + 1) < 0) {
-            fprintf(stderr, "Error write() pressure file %s:", pressure_file[i]);
+            fprintf(stderr, "Error write() pressure file %s:",
+                    pressure_file[i]);
             exit(ERROR_PRESSURE_WRITE);
         }
     }
@@ -88,7 +90,7 @@ void poll_pressure_events() {
 void pressure_event_loop() {
     int event_counter[SZ_IDX];
 
-    for (int i = 0; i < SZ_IDX; i++)  {
+    for (int i = 0; i < SZ_IDX; i++) {
         event_counter[i] = 1;
     }
 
@@ -111,7 +113,8 @@ void pressure_event_loop() {
             if (fds[i].events) {
                 set_time_str();
                 set_content_str(i);
-                printf("%i %s %s\n", event_counter[i]++, pressure_file[i], content_str);
+                printf("%i %s %s %s\n", event_counter[i]++, time_str,
+                        pressure_file[i], content_str);
             } else {
                 fprintf(stderr, "Unrecognized event: 0x%x.\n", fds[i].revents);
                 exit(ERROR_PRESSURE_EVENT_UNK);
@@ -125,7 +128,8 @@ void verify_proc_pressure() {
     int sret = stat(CPU_PRESSURE_FILE, &st);
 
     if (sret == -1) {
-        fprintf(stderr, "To monitor with poll() in Linux, uname -r must report a kernel version of 5.2+\n");
+        fprintf(stderr,
+                "To monitor with poll() in Linux, uname -r must report a kernel version of 5.2+\n");
         exit(ERROR_KERNEL_UNSUPPORTED);
     } else {
         set_time_str();
